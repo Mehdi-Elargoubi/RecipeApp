3f641540ae00c110d469f5b29d31161e5f85e0c9
Mehdi-Elargoubi <mehdi.elargoubi11@gmail.com>
2025-10-06 23:48:25 +0200

    Setup Angular-Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "devopsapi-eilco",
+      "appId": "1:1008955243687:web:58ebd75d44cf66f787af82",
+      "storageBucket": "devopsapi-eilco.firebasestorage.app",
+      "apiKey": "AIzaSyCs0g2r6siR8w4FkhoQT2h4KyS5tATelGc",
+      "authDomain": "devopsapi-eilco.firebaseapp.com",
+      "messagingSenderId": "1008955243687"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions())
   ],
   providers: [],
   bootstrap: [AppComponent]
